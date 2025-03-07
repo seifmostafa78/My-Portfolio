7aafff533383d7b8c326ae7e5859094d04be9fe9
seifmostafa78 <seifmostafaali78@gmail.com>
2025-03-07 22:33:07 +0200

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -116,7 +116,7 @@ const Hero = () => {
       </motion.div>
 
       <div className="absolute inset-0 hidden md:block">
-        <ShinyEffect left={0} top={0} size={1400} />
+        {/* <ShinyEffect left={0} top={0} size={1400} /> */}
       </div>
     </div>
   );
